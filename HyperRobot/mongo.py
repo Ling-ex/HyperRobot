97a35f1491db41863bb4db2a6b071d326027c54a
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2022-11-04 19:07:11 +0700

    Update mongo.py

--- HyperRobot/mongo.py
+++ HyperRobot/mongo.py
@@ -6,20 +6,20 @@ from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
-from EmikoRobot import MONGO_DB_URI
-from EmikoRobot.confing import get_int_key, get_str_key
+from HyperRobot import MONGO_DB_URI
+from HyperRobot.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "EmikoRobot"
+MONGO_DB = "HyperRobot"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["emiexrobot"]
+db = client["HyperRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
