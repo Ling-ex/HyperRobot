adbb5f4d92f3c6ebe99eff826016f2fd6ea1cd45
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2022-11-05 15:05:21 +0700

    Update alive.py

--- HyperRobot/modules/alive.py
+++ HyperRobot/modules/alive.py
@@ -14,11 +14,11 @@ PHOTO = "https://telegra.ph/file/e9391ab2bea553a805974.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hai [{event.sender.first_name}](tg://user?id={event.sender.id}), Dewek Hyper Robot.** \n\n"
-  TEXT += "⚪ **Gua Aktif** \n\n"
-  TEXT += f"⚪ **Tuhan Gua : [ling-zy](https://t.me/excute7)** \n\n"
-  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
-  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "⍟ **Gua Aktif** \n\n"
+  TEXT += f"⍟ **Tuhan Gua : [ling-zy](https://t.me/excute7)** \n\n"
+  TEXT += f"⍟ **Library Version :** `{telever}` \n\n"
+  TEXT += f"⍟ **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"⍟ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Bilang makasih dong, masuk yg ada fi button bawah blok ☺️**"
   BUTTON = [[Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/storyQi"), Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/HyperRobotQ")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
