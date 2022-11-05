ba68a7ccaaa14c17742892fae157c3fa58933ab3
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2022-11-05 23:55:30 +0700

    Update carbon.py

--- HyperRobot/modules/carbon.py
+++ HyperRobot/modules/carbon.py
@@ -4,49 +4,49 @@ from pyrogram import __version__ as z
 from telethon import __version__ as s
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters
-from EmikoRobot import pbot
-from EmikoRobot.utils.errors import capture_err
-from EmikoRobot.utils.functions import make_carbon
+from HyperRobot import pbot
+from HyperRobot.utils.errors import capture_err
+from HyperRobot.utils.functions import make_carbon
 
 
 @pbot.on_message(filters.command("carbon"))
 @capture_err
 async def carbon_func(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("`Reply to a text message to make carbon.`")
+        return await message.reply_text("`Membalas pesan teks untuk membuat karbon.`")
     if not message.reply_to_message.text:
-        return await message.reply_text("`Reply to a text message to make carbon.`")
-    m = await message.reply_text("`Preparing Carbon`")
+        return await message.reply_text("`Membalas pesan teks untuk membuat karbon.`")
+    m = await message.reply_text("`Mempersiapkan Karbon`")
     carbon = await make_carbon(message.reply_to_message.text)
-    await m.edit("`Uploading`")
+    await m.edit("`Mengunggah`")
     await pbot.send_photo(message.chat.id, carbon)
     await m.delete()
     carbon.close()
 
 
-MEMEK = "https://telegra.ph/file/11b5922a33de9968cedfe.jpg"
+MEMEK = "https://telegra.ph/file/e9391ab2bea553a805974.jpg"
 
 @pbot.on_message(filters.command("repo"))
 async def repo(_, message):
     await message.reply_photo(
         photo=MEMEK,
-        caption=f"""✨ **Hey I'm Emiko Robot** 
+        caption=f"""✨ **Hey I'm Hyper Robot** 
 
-**Owner repo : [sena-ex](https://t.me/excrybaby)**
+**Owner repo : [ling-ex](https://t.me/excute7)**
 **Python Version :** `{y()}`
 **Library Version :** `{o}`
 **Telethon Version :** `{s}`
 **Pyrogram Version :** `{z}`
 
-**Create your own with click button bellow.**
+**Buat sendiri dengan klik tombol di bawah.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Repo", url="https://github.com/kennedy-ex/emikorobot"), 
+                        "repo", url="https://t.me/HyperSupportQ"), 
                     InlineKeyboardButton(
-                        "Support", url="https://t.me/emikosupport")
+                        "Channel", url="https://t.me/storyQi")
                 ]
             ]
         )
