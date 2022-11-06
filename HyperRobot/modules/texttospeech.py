9d61c859ce7ca69b9b1274756c4216467280f6c1
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2022-11-06 18:38:27 +0700

    Update texttospeech.py

--- HyperRobot/modules/texttospeech.py
+++ HyperRobot/modules/texttospeech.py
@@ -7,10 +7,10 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from EmikoRobot import *
+from HyperRobot import *
 
-from EmikoRobot import telethn as tbot
-from EmikoRobot.events import register
+from HyperRobot import telethn as tbot
+from HyperRobot.events import register
 
 
 @register(pattern="^/tts (.*)")
