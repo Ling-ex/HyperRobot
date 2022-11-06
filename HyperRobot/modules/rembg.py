f1134e47835a1d9f9b6dd15a3fadc2d43563a04a
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2022-11-06 17:44:25 +0700

    Update rembg.py

--- HyperRobot/modules/rembg.py
+++ HyperRobot/modules/rembg.py
@@ -25,9 +25,9 @@ import requests
 from telethon import types
 from telethon.tl import functions
 
-from EmikoRobot import REM_BG_API_KEY
-from EmikoRobot.events import register
-from EmikoRobot import telethn as tbot
+from HyperRobot import REM_BG_API_KEY
+from HyperRobot.events import register
+from HyperRobot import telethn as tbot
 
 REM_BG_API_KEY = "REM_BG_API_KEY"
 TEMP_DOWNLOAD_DIRECTORY = "./"
