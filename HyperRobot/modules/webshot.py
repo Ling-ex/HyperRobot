1a52f66c3be7146876d26cdf8934039fcb166419
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2022-11-07 12:06:24 +0700

    Update webshot.py

--- HyperRobot/modules/webshot.py
+++ HyperRobot/modules/webshot.py
@@ -20,8 +20,8 @@ SOFTWARE.
 from pyrogram import filters
 from pyrogram.types import Message
 
-from EmikoRobot import pbot as app
-from EmikoRobot.utils.errors import capture_err
+from HyperRobot import pbot as app
+from HyperRobot.utils.errors import capture_err
 
 __mod_name__ = "Webshot​"
 
@@ -32,18 +32,18 @@ async def take_ss(_, message: Message):
     try:
         if len(message.command) != 2:
             return await message.reply_text(
-                "Give A Url To Fetch Screenshot."
+                "Berikan Url Untuk Mengambil Screenshott."
             )
         url = message.text.split(None, 1)[1]
-        m = await message.reply_text("**Taking Screenshot**")
-        await m.edit("**Uploading**")
+        m = await message.reply_text("**Mengambil Screenshott**")
+        await m.edit("**Mengunggah**")
         try:
             await message.reply_photo(
                 photo=f"https://webshot.amanoteam.com/print?q={url}",
                 quote=False,
             )
         except TypeError:
-            return await m.edit("No Such Website.")
+            return await m.edit("Tidak Ada Situs Web Tersebut.")
         await m.delete()
     except Exception as e:
         await message.reply_text(str(e))
